5f9fe48f8c973b23eb30733f93eec4d9ee8719f6
Roberto Noorduijn Londono <roberto@MacBook-Pro-van-Roberto.local>
2022-01-21 10:58:36 +0100

    added tutorial dag

---
+++ dags/tutorial.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+import datetime as dt
+
+default_args = {
+    "owner": "roberto",
+    "start_date": dt.datetime(2021, 7, 28),
+    "retries": 1,
+    "retry_delay": dt.timedelta(minutes=5),
+}
+
+with DAG(
+    "simple_example",
+    description="Simple example DAG",
+    default_args=default_args,
+    schedule_interval=dt.timedelta(seconds=5),
+) as dag:
+
+    task1 = BashOperator(
+        task_id="print_hello",
+        bash_command="echo 'Greetings!. The date and time are'",
+    )
+
+    task2 = BashOperator(
+        task_id="print_date",
+        bash_command="date",
+    )
+
+    task1 >> task2
